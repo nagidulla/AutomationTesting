634e3698a32d1e5a26a412d06966e2f977c00627
nagidulla <83960337+nagidulla@users.noreply.github.com>
2022-10-04 16:01:18 +0530

    Update AutomationScript.java

--- src/test/AutomationScript.java
+++ src/test/AutomationScript.java
@@ -8,7 +8,7 @@ public class AutomationScript {
  public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\Nagendra\\Downloads\\chromedriver.exe");
 	 WebDriver driver = new ChromeDriver();
-	 driver.get("https://adactinhotelapp.com");
+	 driver.navigate().to("https://adactinhotelapp.com");
 	 driver.manage().window().maximize();
 	 driver.findElement(By.id("username")).sendKeys("nagendra123");
 	 driver.findElement(By.id("password")).sendKeys("25121999");
