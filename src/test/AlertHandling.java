ee78155a5e93ebc4925573b7bebe3fb8d703d25a
nagidulla <sai143050@gmail.com>
2022-10-04 16:51:22 +0530

    AlertHandling modified

--- src/test/AlertHandling.java
+++ src/test/AlertHandling.java
@@ -1,9 +1,24 @@
 package test;
 
+import java.time.Duration;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AlertHandling {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Nagendra\\Downloads\\chromedriver.exe");
+		 WebDriver driver = new ChromeDriver();
+		 driver.get("https://demoqa.com/alerts");
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 driver.findElement(By.id("alertButton")).click();
+		 Alert al = driver.switchTo().alert();
+		 System.out.println(al.getText());
+		 al.accept();
+		
 
 	}
 
